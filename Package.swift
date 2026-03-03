4431a559937082bebc4bab2497108ac6a7ec6574
Philipp Nagy <philipp.nagy@tum.de>
2026-03-03 11:37:16 -0800

    fix spezi healthkit version

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
         .package(url: "https://github.com/StanfordSpezi/Spezi.git", from: "1.10.1"),
         .package(url: "https://github.com/StanfordSpezi/SpeziVapor.git", .upToNextMajor(from: "0.1.0")),
         .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", from: "2.7.3"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziHealthKit.git", from: "1.40"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziHealthKit.git", from: "1.4.0"),
         .package(url: "https://github.com/StanfordSpezi/SpeziStudy.git", branch: "localized-metadata") // TODO
     ] + swiftLintPackage(),
     targets: [
